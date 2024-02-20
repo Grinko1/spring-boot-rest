284cdbde6015d8831cb455817d46df4878740682
Grinko1 <nadenka64013730@gmail.com>
2024-02-20 11:29:27 +0900

    add entity manager

--- src/main/java/com/spring/boot/dao/EmployeeDaoImpl.java
+++ src/main/java/com/spring/boot/dao/EmployeeDaoImpl.java
@@ -2,6 +2,7 @@ package com.spring.boot.dao;
 
 import com.spring.boot.entity.Employee;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,29 +17,33 @@ public class EmployeeDaoImpl implements EmployeeDao {
     @Override
 
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
+        // old
+//        Session session = entityManager.unwrap(Session.class);
+//        List<Employee> emps = session.createQuery("from Employee", Employee.class).getResultList();
 
-        List<Employee> emps = session.createQuery("from Employee", Employee.class).getResultList();
 
+       List<Employee> emps = entityManager.createQuery("from Employee").getResultList();
         return emps;
     }
 
 
     @Override
     public void saveEmp(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(employee);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(employee);
+
+        entityManager.merge(employee);
     }
 
     @Override
     public Employee findById(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        return session.get(Employee.class, id);
+        return entityManager.find(Employee.class, id);
     }
 
     @Override
     public void deleteEmp(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.delete(employee);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.delete(employee);
+        entityManager.remove(employee);
     }
 }
